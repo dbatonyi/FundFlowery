94b0ab919fdba1af78b8a86bcf1d42abe36c6e04
Donát Bátonyi <dbatonyi@gmail.com>
2023-05-08 18:10:38 +0200

    FundFlowery - Desktop design changed (0.9.1)

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,11 +5,13 @@ import MainNavigation from "./MainNavigation";
 const Header = ({ auth, logout }) => {
   return (
     <header className="fund-flowery__header">
-      <div className="fund-flowery__header--logo">
-        <Link href="/">FundFlowery</Link>
+      <div className="fund-flowery__header--container">
+        <div className="fund-flowery__header--logo">
+          <Link href="/">FundFlowery</Link>
+        </div>
+        <MainNavigation auth={auth} logout={logout} />
+        <LanguageSwitcher />
       </div>
-      <MainNavigation auth={auth} logout={logout} />
-      <LanguageSwitcher />
     </header>
   );
 };

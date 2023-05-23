5f936b59da3fa871f153b7c47b555f5b1d52512f
Donát Bátonyi <dbatonyi@gmail.com>
2023-05-23 15:19:14 +0200

    FundFlowery - Unix socket connection support added (0.9.6)

--- api/db/sequelize.js
+++ api/db/sequelize.js
@@ -1,9 +1,22 @@
 const config = require("../config")["database"];
 
-module.exports = {
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
-  dialect: config.dialect,
-};
+if (config.socketPath) {
+  module.exports = {
+    username: config.username,
+    password: config.password,
+    database: config.database,
+    host: config.socketPath,
+    dialect: config.dialect,
+    dialectOptions: {
+      socketPath: config.socketPath,
+    },
+  };
+} else {
+  module.exports = {
+    username: config.username,
+    password: config.password,
+    database: config.database,
+    host: config.host,
+    dialect: config.dialect,
+  };
+}
